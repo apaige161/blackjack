f802175caea26311653f4fea8453926d87c54d30
Alex Paige <apaige161@gmail.com>
2020-05-13 15:15:27 -0400

    added two random dice

---
+++ blackjack/Dice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace blackjack
+{
+    public class Dice
+    {
+
+        //creates a new random card suit
+        public static int NewDie()
+        {
+            Random random = new Random();
+            int dieVal = random.Next(1, 7);
+
+            return dieVal;
+        }
+
+        public Dice()
+        {
+        }
+    }
+}
